64a9d21f07796e74b6128c689f8969cd0d688b80
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-12-02 23:12:23 +0600

    Version 1.0.1

---
+++ src/features/Card/ui/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface CardProps {
+  image: string;
+  text: string;
+}
+
+export const Card: React.FC<CardProps> = ({ image, text }) => {
+  return (
+    <div className="card">
+      <img src={image} className="card__image" alt="card-image" />
+      <span className="card__text">{text}</span>
+    </div>
+  );
+};
---
+++ src/widgets/screens/Home/MainScreen/ui/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@features/Header/ui";
+
+export const MainScreen = () => {
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <h1>SUN GRAIN</h1>
+        <p className="paragraph text-center">
+          Ваш Надежный Партнер в Поставке Качественной Продукции
+        </p>
+      </main>
+    </>
+  );
+};
