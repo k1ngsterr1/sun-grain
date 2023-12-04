04dd8fd1445db00959162f008597195d49728b2e
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-12-04 22:35:36 +0600

    Version 1.1.3

---
+++ src/widgets/Form/ui/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Input } from "@shared/ui/Input";
+
+export const Form = () => {
+  return (
+    <form className="form">
+      <Input placeholder="Введите ваше имя" />
+    </form>
+  );
+};
