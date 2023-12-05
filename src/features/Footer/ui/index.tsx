0f043e342137453e9cba83abbb8b556c0a5b0903
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-12-05 19:06:57 +0600

    Version 1.1.8

--- src/features/Footer/ui/index.tsx
+++ src/features/Footer/ui/index.tsx
@@ -1,5 +1,47 @@
-import React from "react";
+import { Logo } from "@shared/ui/Logo";
+import { Link } from "react-scroll";
+
+import logo from "@assets/images/spark_logo.svg";
+
+import "./styles.scss";
 
 export const Footer = () => {
-  return <footer className="footer">index</footer>;
+  function SparkStudio() {
+    window.open("https://sparkstudio.kz/", "_blank");
+  }
+
+  return (
+    <footer className="footer">
+      <div className="footer__mob flex flex-col items-center">
+        <Logo size={"big"} />
+        <Link to="main" className="footer__mob__link mt-8">
+          Наша продукция
+        </Link>
+        <Link to="main" className="footer__mob__link mt-4">
+          О компании{" "}
+        </Link>
+        <Link to="main" className="footer__mob__link mt-4">
+          Каталог
+        </Link>
+        <Link to="main" className="footer__mob__link mt-4">
+          Контакты
+        </Link>
+        <a href="tel:+77066018826" className="footer__mob__contact-link mt-5">
+          +7 706 601 88-26
+        </a>
+        <a
+          href="mailto:ceo@sungrain.kz"
+          className="footer__mob__contact-link mt-2"
+        >
+          ceo@sungrain.kz
+        </a>
+        <img
+          src={logo}
+          alt="logo"
+          className="footer__mob__logo mt-10 mb-10"
+          onClick={SparkStudio}
+        />
+      </div>
+    </footer>
+  );
 };
--- src/widgets/Form/ui/index.tsx
+++ src/widgets/Form/ui/index.tsx
@@ -3,7 +3,7 @@ import { Input } from "@shared/ui/Input";
 
 export const Form = () => {
   return (
-    <form className="form w-full flex flex-col items-center">
+    <form className="form w-full flex flex-col items-center mb-10">
       <div className="w-full flex justify-center mt-8">
         <Input placeholder="Введите ваше имя" />
       </div>
