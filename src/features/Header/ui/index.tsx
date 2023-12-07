1a03488eb5d08b03d161b6df8b0bc84ad8653013
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-12-07 11:48:19 +0600

    Version 1.2.3

--- src/features/Header/ui/index.tsx
+++ src/features/Header/ui/index.tsx
@@ -2,6 +2,8 @@ import Hamburger from "hamburger-react";
 import { Logo } from "../../../shared/ui/Logo";
 import { useSelector } from "react-redux";
 import { useOpenMenu } from "@shared/lib/hooks/useOpenMenu";
+import { Link } from "react-scroll";
+
 import "./styles.scss";
 
 export const Header = () => {
@@ -9,11 +11,30 @@ export const Header = () => {
   const openMenu = useOpenMenu(isMenuOpen);
 
   return (
-    <header className="header flex justify-between items-center">
-      <Logo />
-      <div className="header__hamburger-btn">
-        <Hamburger color="#e55b12" onToggle={openMenu} toggled={isMenuOpen} />
-      </div>
-    </header>
+    <>
+      <header className="header flex justify-between items-center">
+        <Logo />
+        <div className="header__hamburger-btn">
+          <Hamburger color="#e55b12" onToggle={openMenu} toggled={isMenuOpen} />
+        </div>
+      </header>
+      <header className="header-pc flex justify-between items-center">
+        <Logo />
+        <nav className="header-pc__navigation">
+          <Link className="header-pc__navigation__link" to="main">
+            Наша продукция
+          </Link>
+          <Link className="header-pc__navigation__link" to="about">
+            О компании
+          </Link>
+          <Link className="header-pc__navigation__link" to="catalogue">
+            Каталог
+          </Link>
+          <Link className="header-pc__navigation__link" to="contacts">
+            Контакты
+          </Link>
+        </nav>
+      </header>
+    </>
   );
 };
